0e7e652abebc04fd4a78560961f683405ff64ba0
MalenaMaffei <malenamaffei@hotmail.com>
2017-05-31 16:29:37 -0300

    stub de cola de paquetes

--- client/VistasSDL/ColaPaquetes.cpp
+++ client/VistasSDL/ColaPaquetes.cpp
@@ -1,1 +1,10 @@
 #include "Header Files/ColaPaquetes.h"
+
+void ColaPaquetes::encolar(Paquete& paquete){
+    paquetes.push(paquete);
+}
+
+Paquete ColaPaquetes::desencolar() {
+    Paquete paquete = paquetes.front();
+    paquetes.pop();
+}
